4dc6f24cb70707f62d87fcf786300d0a4f36b7f3
liangshan <18382242400@163.com>
2019-10-22 17:20:30 +0800

    修改pom文件

--- ls-security-demo/src/main/java/com/ls/security/App.java
+++ ls-security-demo/src/main/java/com/ls/security/App.java
@@ -1,13 +1,25 @@
 package com.ls.security;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@RestController
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
     }
 }
